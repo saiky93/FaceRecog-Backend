66e853c97366e333b3796adc5e82419e9f31df57
saiky93 <38883382+saiky93@users.noreply.github.com>
2018-05-22 08:54:48 -0400

    pic  coming back

--- src/app/services/training.service.ts
+++ src/app/services/training.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response,RequestOptions, Headers } from '@angular/http';
 import 'rxjs/Rx';
+import { ContentType, ResponseType } from '@angular/http/src/enums';
 
 @Injectable()
 export class TrainingService{
@@ -13,6 +14,14 @@ training(id)
 {
     
     return this.http.get('http://127.0.0.1:5000/takePictures/'+id);
+}
+
+prediction(dataURI){
+    let bodyString = JSON.stringify({imguri: dataURI})
+    console.log(bodyString);
+    let headers = new Headers({'Content-Type':'application/json; charset=utf-8' , 'DataType':'json'});
+    let options = new RequestOptions({headers:headers});
+    return this.http.post('http://127.0.0.1:5000/predict',bodyString,options);
 }
 }
 
--- src/assets/imagedetection/faceRecognition.js
+++ src/assets/imagedetection/faceRecognition.js
@@ -71,5 +71,3 @@ $(document).ready(function() {
         });
     });
 });
-
-
